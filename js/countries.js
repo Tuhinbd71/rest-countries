94e27e180ee7bbdc8fd4cabfbd1ec034d3d12dfb
Tuhinbd71 <m.tuhin02bd@gmail.com>
2023-06-06 22:54:01 +0600

    country commit

--- js/countries.js
+++ js/countries.js
@@ -5,10 +5,12 @@ const loadCountries = () => {
 }
 const displayCountries = countries => {
     // console.log(countries)
-    const countriesHTML = countries.map(country => getCountriesHTML(country));
-    console.log(countriesHTML)
+    const countriesHTML = countries.map(country => getCountryHTML(country));
+    // console.log(countriesHTML)
+    const container = document.getElementById("countries");
+    container.innerHTML = countriesHTML;
 }
-const getCountriesHTML = country => {
+const getCountryHTML = country => {
     return `
         <div>
             <h2>${country.name.common}</h2>
